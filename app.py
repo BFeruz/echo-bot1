41a3c2bb0cbce9d008604eb0393cd70a6f4a6b9f
BFeruz <bahodirovferuz873@gmail.com>
2024-01-30 16:06:19 +0500

    text

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 import telegram
 
 app = Flask(__name__)
 
 TOKEN = '6926950017:AAEjcggDcurUZ-j2H4SgRglTG4mZjTNsJo4'
-bot = telegram.bot(TOKEN)
+bot = telegram.Bot(TOKEN)
 chat_id = '5534265722'
 
-@app.route('/')
-
-def index():
-    print('join')
+@app.route('/', methods=['POST'])
+def user():
+    data = request.get_json() 
+    print(data)
     bot.send_message(chat_id=chat_id, text='Hello!!')
     return 'join'
+if __name__=="__main__":
+    app.run(debug=True)
