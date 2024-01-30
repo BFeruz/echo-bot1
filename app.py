5349e13ba10ea346c55209badc4550edba629df9
BFeruz <bahodirovferuz873@gmail.com>
2024-01-30 14:34:31 +0500

    as

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ TOKEN = '6926950017:AAEjcggDcurUZ-j2H4SgRglTG4mZjTNsJo4'
 bot = telegram.bot(TOKEN)
 chat_id = '5534265722'
 
-@app.route('/', methods=['POST'])
+@app.route('/')
+
 def index():
-    print('index page')
-    bot.send_message(chat_id=chat_id, text='Hello World!!!')
-    return 'index page'
+    print('join')
+    bot.send_message(chat_id=chat_id, text='Hello!!')
+    return 'join'
