dafd12e46773ec39b2d0f5589390741490179c4d
BFeruz <bahodirovferuz873@gmail.com>
2024-01-30 14:30:56 +0500

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-selam
+from flask import Flask
+import telegram
+
+app = Flask(__name__)
+
+TOKEN = '6926950017:AAEjcggDcurUZ-j2H4SgRglTG4mZjTNsJo4'
+bot = telegram.bot(TOKEN)
+chat_id = '5534265722'
+
+@app.route('/', methods=['POST'])
+def index():
+    print('index page')
+    bot.send_message(chat_id=chat_id, text='Hello World!!!')
+    return 'index page'
